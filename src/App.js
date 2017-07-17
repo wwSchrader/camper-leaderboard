a98fc96a8399198ca1947f52276f63e2edee67fd
wwSchrader <wwschrader@gmail.com>
2017-07-16 22:07:39 -0700

    Add fetch method to get user data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        userArray: ''
+      };
+    }
+
+    componentDidMount() {
+      var url = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+      var that = this;
+
+      fetch(url)
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        that.setState({ userArray: data});
+        console.log(that.state.userArray)
+      });
+    }
+
   render() {
+
     return (
       <div className="App">
         <table>
